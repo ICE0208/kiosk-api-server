0f57d637626ee78cf9e4edc5d260eaf851c9a229
ICE0208 <doky3307@naver.com>
2023-08-14 18:08:33 +0900

    카카오 페이 결제 API 완료

--- src/controllers/apiController.js
+++
@@ -1,9 +0,0 @@
-import { async } from "regenerator-runtime";
-
-export const readyPay = async (req, res) => {
-  return res.status(200).json({ ok: true });
-};
-
-export const approvePay = async (req, res) => {
-  return res.status(200).json({ ok: true });
-};
--- src/routers/payRouter.js
+++ src/routers/payRouter.js
@@ -1,7 +1,21 @@
 import express from "express";
-import { approvePay, readyPay } from "../controllers/apiController";
+import {
+  approvePay,
+  payCancel,
+  payFail,
+  payFinalFail,
+  payFinalSuccess,
+  paySuccess,
+  readyPay,
+} from "../controllers/payController";
 
 const payRouter = express.Router();
+
+payRouter.get("/success", paySuccess);
+payRouter.get("/cancel", payCancel);
+payRouter.get("/fali", payFail);
+payRouter.get("/finalSuccess", payFinalSuccess);
+payRouter.get("/finalFail", payFinalFail);
 
 payRouter.post("/ready", readyPay);
 payRouter.post("/approve", approvePay);
